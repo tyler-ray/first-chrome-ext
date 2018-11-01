5927f9f22c739e81b05368d122bb9e07b16e7de2
jtylerray <jtray@outlook.es>
2018-11-01 12:19:57 -0600

    first chrome ext based on coding train

---
+++ content.js
@@ -0,0 +1,15 @@
+console.log('chrome ext go?')
+
+
+
+chrome.runtime.onMessage.addListener(gotMessage)
+function gotMessage(message, sender, sendResponse){
+ console.log(message.txt)
+ if (message.txt === 'hello'){
+    let paragraphs = document.getElementsByTagName('p')
+    for (el of paragraphs){
+        el.style['background-color']='#bada55';
+    
+    }
+ }
+}
