3619c8777b07794ae119d165831c3920011c5cef
jtylerray <jtray@outlook.es>
2018-11-01 12:25:24 -0600

    on all urls!

--- content.js
+++ content.js
@@ -5,11 +5,12 @@ console.log('chrome ext go?')
 chrome.runtime.onMessage.addListener(gotMessage)
 function gotMessage(message, sender, sendResponse){
  console.log(message.txt)
+ 
  if (message.txt === 'hello'){
     let paragraphs = document.getElementsByTagName('p')
     for (el of paragraphs){
         el.style['background-color']='#bada55';
-    
     }
  }
+ console.log('paragraphs changed!')
 }
